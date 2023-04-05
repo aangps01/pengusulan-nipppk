0239a4c66e384160a9f7b8867926e2cbb623541c
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2023-04-06 02:26:42 +0800

    add nik to user model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,20 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Aang Pangantyas Sampurna',
+            'nik' => '1234567890123456',
+            'email' =>'1234567890123456@example.com',
+            'password' => bcrypt('1234567890123456'),
+            'role' => 1,
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'nik' => 'admin',
+            'email' =>'admin@example.com',
+            'password' => bcrypt('admin'),
+            'role' => 2,
+        ]);
     }
 }
