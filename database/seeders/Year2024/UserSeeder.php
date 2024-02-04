82c6cf8f7f92ea7e5c0bb646a2006ff0b62f62ad
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2024-02-04 23:55:44 +0800

    new data 2024

--- database/seeders/Year2024/UserSeeder.php
+++ database/seeders/Year2024/UserSeeder.php
@@ -28,24 +28,44 @@ class UserSeeder extends Seeder
         try{
             foreach($pegawai_teknis as $pegawai)
             {
-                User::updateOrCreate([
-                    'nik' => $pegawai['NIK'],
-                ], [
-                    'name' => $pegawai['NAMA SESUAI IJAZAH'],
-                    'email' => $pegawai['NIK'] . '@example.com',
-                    'password' => Hash::make($pegawai['NIK']),
-                    'role' => 1,
-                    'nomor_peserta' => $pegawai['NO PESERTA'],
-                    'gelar_belakang' => $pegawai['GELAR'],
-                    'tempat_lahir' => $pegawai['TEMPAT LAHIR'],
-                    'tanggal_lahir' => $pegawai['TANGGAL LAHIR'],
-                    'jenis_kelamin' => $pegawai['JENIS KELAMIN'],
-                    'pendidikan' => $pegawai['PENDIDIKAN'],
-                    'jabatan_dilamar' => $pegawai['JABATAN YG DILAMAR'],
-                    'unit_kerja' => $pegawai['UNIT KERJA'],
-                    'tipe' => 'teknis',
-                    'tahun' => 2024
-                ]);
+                $user = User::where('nik', $pegawai['NIK'])->first();
+                if($user){
+                    $user->update([
+                        'name' => $pegawai['NAMA SESUAI IJAZAH'],
+                        'email' => $pegawai['NIK'] . '@example.com',
+                        'role' => 1,
+                        'nomor_peserta' => $pegawai['NO PESERTA'],
+                        'gelar_depan' => $pegawai['GELAR DEPAN'],
+                        'gelar_belakang' => $pegawai['GELAR'],
+                        'tempat_lahir' => $pegawai['TEMPAT LAHIR'],
+                        'tanggal_lahir' => $pegawai['TANGGAL LAHIR'],
+                        'jenis_kelamin' => $pegawai['JENIS KELAMIN'],
+                        'pendidikan' => $pegawai['PENDIDIKAN'],
+                        'jabatan_dilamar' => $pegawai['JABATAN YG DILAMAR'],
+                        'unit_kerja' => $pegawai['UNIT KERJA'],
+                        'tipe' => 'teknis',
+                        'tahun' => 2024
+                    ]);
+                }else{
+                    User::create([
+                        'nik' => $pegawai['NIK'],
+                        'name' => $pegawai['NAMA SESUAI IJAZAH'],
+                        'email' => $pegawai['NIK'] . '@example.com',
+                        'password' => Hash::make($pegawai['NIK']),
+                        'role' => 1,
+                        'nomor_peserta' => $pegawai['NO PESERTA'],
+                        'gelar_depan' => $pegawai['GELAR DEPAN'],
+                        'gelar_belakang' => $pegawai['GELAR'],
+                        'tempat_lahir' => $pegawai['TEMPAT LAHIR'],
+                        'tanggal_lahir' => $pegawai['TANGGAL LAHIR'],
+                        'jenis_kelamin' => $pegawai['JENIS KELAMIN'],
+                        'pendidikan' => $pegawai['PENDIDIKAN'],
+                        'jabatan_dilamar' => $pegawai['JABATAN YG DILAMAR'],
+                        'unit_kerja' => $pegawai['UNIT KERJA'],
+                        'tipe' => 'teknis',
+                        'tahun' => 2024
+                    ]);
+                }
             }
             DB::commit();
         }catch(\Exception $e){
@@ -57,24 +77,42 @@ class UserSeeder extends Seeder
         DB::beginTransaction();
         try {
             foreach ($pegawai_guru as $pegawai) {
-                User::updateOrCreate([
-                    'nik' => $pegawai['NIK'],
-                ], [
-                    'name' => $pegawai['NAMA SESUAI IJAZAH'],
-                    'email' => $pegawai['NIK'] . '@example.com',
-                    'password' => Hash::make($pegawai['NIK']),
-                    'role' => 1,
-                    'nomor_peserta' => $pegawai['NO PESERTA'],
-                    'gelar_belakang' => $pegawai['GELAR BELAKANG'],
-                    'tempat_lahir' => $pegawai['TEMPAT LAHIR'],
-                    'tanggal_lahir' => Carbon::createFromFormat('d-m-Y', $pegawai['TANGGAL LAHIR'])->format('Y-m-d'),
-                    'jenis_kelamin' => $pegawai['JENIS KELAMIN'],
-                    'pendidikan' => $pegawai['PENDIDIKAN'],
-                    'jabatan_dilamar' => $pegawai['JABATAN YG DILAMAR'],
-                    'unit_kerja' => $pegawai['UNIT KERJA'],
-                    'tipe' => 'guru',
-                    'tahun' => 2024
-                ]);
+                $user = User::where('nik', $pegawai['NIK'])->first();
+                if ($user) {
+                    $user->update([
+                        'name' => $pegawai['NAMA SESUAI IJAZAH'],
+                        'email' => $pegawai['NIK'] . '@example.com',
+                        'role' => 1,
+                        'nomor_peserta' => $pegawai['NO PESERTA'],
+                        'gelar_belakang' => $pegawai['GELAR BELAKANG'],
+                        'tempat_lahir' => $pegawai['TEMPAT LAHIR'],
+                        'tanggal_lahir' => $pegawai['TANGGAL LAHIR'],
+                        'jenis_kelamin' => $pegawai['JENIS KELAMIN'],
+                        'pendidikan' => $pegawai['PENDIDIKAN'],
+                        'jabatan_dilamar' => $pegawai['JABATAN YG DILAMAR'],
+                        'unit_kerja' => $pegawai['UNIT KERJA'],
+                        'tipe' => 'guru',
+                        'tahun' => 2024
+                    ]);
+                }else{
+                    User::create([
+                        'nik' => $pegawai['NIK'],
+                        'name' => $pegawai['NAMA SESUAI IJAZAH'],
+                        'email' => $pegawai['NIK'] . '@example.com',
+                        'password' => Hash::make($pegawai['NIK']),
+                        'role' => 1,
+                        'nomor_peserta' => $pegawai['NO PESERTA'],
+                        'gelar_belakang' => $pegawai['GELAR BELAKANG'],
+                        'tempat_lahir' => $pegawai['TEMPAT LAHIR'],
+                        'tanggal_lahir' => $pegawai['TANGGAL LAHIR'],
+                        'jenis_kelamin' => $pegawai['JENIS KELAMIN'],
+                        'pendidikan' => $pegawai['PENDIDIKAN'],
+                        'jabatan_dilamar' => $pegawai['JABATAN YG DILAMAR'],
+                        'unit_kerja' => $pegawai['UNIT KERJA'],
+                        'tipe' => 'guru',
+                        'tahun' => 2024
+                    ]);
+                }
             }
             DB::commit();
         } catch (\Exception $e) {
@@ -86,25 +124,44 @@ class UserSeeder extends Seeder
         DB::beginTransaction();
         try {
             foreach ($pegawai_nakes as $pegawai) {
-                User::updateOrCreate([
-                    'nik' => $pegawai['NIK'],
-                ], [
-                    'name' => $pegawai['NAMA SESUAI IJAZAH'],
-                    'email' => $pegawai['NIK'] . '@example.com',
-                    'password' => Hash::make($pegawai['NIK']),
-                    'role' => 1,
-                    'nomor_peserta' => $pegawai['NO PESERTA'],
-                    'gelar_depan' => $pegawai['GELAR DEPAN'],
-                    'gelar_belakang' => $pegawai['GELAR BELAKANG'],
-                    'tempat_lahir' => $pegawai['TEMPAT LAHIR'],
-                    'tanggal_lahir' => $pegawai['TANGGAL LAHIR'],
-                    'jenis_kelamin' => $pegawai['JENIS KELAMIN'],
-                    'pendidikan' => $pegawai['PENDIDIKAN'],
-                    'jabatan_dilamar' => $pegawai['JABATAN YG DILAMAR'],
-                    'unit_kerja' => $pegawai['UNIT KERJA'],
-                    'tipe' => 'nakes',
-                    'tahun' => 2024
-                ]);
+                $user = User::where('nik', $pegawai['NIK'])->first();
+                if($user){
+                    $user->update([
+                        'name' => $pegawai['NAMA SESUAI IJAZAH'],
+                        'email' => $pegawai['NIK'] . '@example.com',
+                        'role' => 1,
+                        'nomor_peserta' => $pegawai['NO PESERTA'],
+                        'gelar_depan' => $pegawai['GELAR DEPAN'],
+                        'gelar_belakang' => $pegawai['GELAR BELAKANG'],
+                        'tempat_lahir' => $pegawai['TEMPAT LAHIR'],
+                        'tanggal_lahir' => $pegawai['TANGGAL LAHIR'],
+                        'jenis_kelamin' => $pegawai['JENIS KELAMIN'],
+                        'pendidikan' => $pegawai['PENDIDIKAN'],
+                        'jabatan_dilamar' => $pegawai['JABATAN YG DILAMAR'],
+                        'unit_kerja' => $pegawai['UNIT KERJA'],
+                        'tipe' => 'nakes',
+                        'tahun' => 2024
+                    ]);
+                }else{
+                    User::create([
+                        'nik' => $pegawai['NIK'],
+                        'name' => $pegawai['NAMA SESUAI IJAZAH'],
+                        'email' => $pegawai['NIK'] . '@example.com',
+                        'password' => Hash::make($pegawai['NIK']),
+                        'role' => 1,
+                        'nomor_peserta' => $pegawai['NO PESERTA'],
+                        'gelar_depan' => $pegawai['GELAR DEPAN'],
+                        'gelar_belakang' => $pegawai['GELAR BELAKANG'],
+                        'tempat_lahir' => $pegawai['TEMPAT LAHIR'],
+                        'tanggal_lahir' => $pegawai['TANGGAL LAHIR'],
+                        'jenis_kelamin' => $pegawai['JENIS KELAMIN'],
+                        'pendidikan' => $pegawai['PENDIDIKAN'],
+                        'jabatan_dilamar' => $pegawai['JABATAN YG DILAMAR'],
+                        'unit_kerja' => $pegawai['UNIT KERJA'],
+                        'tipe' => 'nakes',
+                        'tahun' => 2024
+                    ]);
+                }
             }
             DB::commit();
         } catch (\Exception $e) {
